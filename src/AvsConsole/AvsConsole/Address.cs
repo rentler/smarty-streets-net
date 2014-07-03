abefe167991931e268924189a61cd41f589bb398
Dustin Dahl <dustin.dahl@gmail.com>
2014-07-03 15:16:48 -0700

    Initial commit. It's just a test project in a console app for now. I'll start parting everything out once a few more things have congealed proper.

---
+++ src/AvsConsole/AvsConsole/Address.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AvsConsole
+{
+	public class Address
+	{
+		public string Address1 { get; set; }
+		public string Address2 { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string Zip { get; set; }
+		public string FullAddress
+		{
+			get
+			{
+				string address = string.Empty;
+
+				//address line
+				if (!string.IsNullOrWhiteSpace(this.Address1))
+				{
+					address += this.Address1.Trim();
+					if (!string.IsNullOrWhiteSpace(this.Address2))
+						address += " " + this.Address2.Trim();
+				}
+
+				//city
+				if (!string.IsNullOrWhiteSpace(this.City))
+					if (address.Length > 0)
+						address += ", " + this.City.Trim();
+					else
+						address += this.City.Trim();
+
+				//state
+				if (!string.IsNullOrWhiteSpace(this.State))
+					if (address.Length > 0)
+						address += ", " + this.State.Trim();
+					else
+						address += this.State.Trim();
+
+				if (!string.IsNullOrWhiteSpace(this.Zip))
+					if (address.Length > 0)
+						address += " " + this.Zip.Trim();
+					else
+						address += this.Zip.Trim();
+
+				return address;
+			}
+		}
+
+		public void Clean()
+		{
+			//trim spaces
+			this.Address1 = SafeTrim(this.Address1);
+			this.Address2 = SafeTrim(this.Address2);
+			this.City = SafeTrim(this.City);
+			this.State = SafeTrim(this.State);
+			this.Zip = SafeTrim(this.Zip);
+
+			//common stuff
+			this.Address1 = SafeReplace(this.Address1, "\\.");
+			this.City = SafeReplace(this.City, "\\.");
+		}
+
+		string SafeReplace(string item, string match, string replace = "")
+		{
+			if (!string.IsNullOrWhiteSpace(item))
+				item = Regex.Replace(item, match, replace, RegexOptions.IgnoreCase);
+
+			return item;
+		}
+
+		string SafeTrim(string item)
+		{
+			if (!string.IsNullOrWhiteSpace(item))
+				item = item.Trim();
+
+			return item;
+		}
+	}
+}
---
+++ src/AvsConsole/AvsConsole/SmartyStreetsCityStateZipLookup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvsConsole
+{
+	public class SmartyStreetsCityStateZipLookup
+	{
+		public List<CityState> city_states { get; set; }
+		public List<Zipcode> zipcodes { get; set; }
+	}
+
+	public class CityState
+	{
+		public string city { get; set; }
+		public string state_abbreviation { get; set; }
+		public string state { get; set; }
+	}
+
+	public class Zipcode
+	{
+		public string zipcode { get; set; }
+		public string zipcode_type { get; set; }
+		public string county_fips { get; set; }
+		public string county_name { get; set; }
+		public double latitude { get; set; }
+		public double longitude { get; set; }
+	}
+}
